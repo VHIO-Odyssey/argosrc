d01c60ec264f0bdf39bf1f07881f642d32b5aa7f
garcia-galea <eduardogarcia@vhio.net>
2025-08-01 09:37:48 +0200

    get_conditions improvements

--- R/completeness.R
+++ R/completeness.R
@@ -9,15 +9,10 @@ filter_condition <- function(data_frame, condition) {
 }
 
 # Helper functions to create a conditions_list from redcap metadata.
-get_conditions_from_metadata <- function(data_frame,
-                                         metadata,
-                                         missing_codes) {
+get_conditions_from_metadata <- function(metadata, missing_codes) {
 
   needed_meta <- metadata |>
-    dplyr::filter(
-      .data$field_name %in% names(data_frame),
-      !is.na(.data$branching_logic)
-    )
+    dplyr::filter(!is.na(.data$branching_logic))
 
   missing_value <- stringr::str_c(
     missing_codes$raw_value, collapse = "|")
@@ -28,7 +23,7 @@ get_conditions_from_metadata <- function(data_frame,
       dplyr::filter(
         stringr::str_detect(
           .data$branching_logic,
-          "\\[.+\\]\\[.+\\]|event-name|current-instance|user-role-name"
+          "\\[.+\\]\\[.+\\]|current-instance|user-role-name"
         )
       ) |> dplyr::pull("field_name")
 
@@ -45,9 +40,15 @@ get_conditions_from_metadata <- function(data_frame,
       dplyr::select("field_name", "branching_logic") |>
       dplyr::mutate(
         # RedCap logic is translated into R languaje
-        r_branch = stringr::str_replace_all(
-          .data$branching_logic,  missing_value, "user_na"
-        ) |>
+        r_branch =
+          stringr::str_replace_all(
+            .data$branching_logic, "event-name", "redcap_event_name"
+          ) |>
+          # Any upper case AND / OR is lower cased to avoid motential confusion with
+          # the missing data codes.
+          stringr::str_replace_all(" AND ", " and ") |>
+          stringr::str_replace_all( " OR ", " or ") |>
+          stringr::str_replace_all(missing_value, "user_na") |>
           # Checkbox variables to especific check box column
           stringr::str_replace_all( "\\((\\d+)\\)", "___\\1") |>
           stringr::str_replace_all(
@@ -64,8 +65,8 @@ get_conditions_from_metadata <- function(data_frame,
           stringr::str_remove_all("\\[|\\]") |>
           stringr::str_replace_all("=", "==") |>
           stringr::str_replace_all("<>", "!=") |>
-          stringr::str_replace_all(" or ?| ?or ", " | ") |>
-          stringr::str_replace_all(" and ?| ?and ", " & ") |>
+          stringr::str_replace_all(" or ", " | ") |>
+          stringr::str_replace_all(" and ", " & ") |>
           # Delete possible duplicates of is_user_na
           stringr::str_replace_all("(.*labelled::is_user_na.+)\\1+", "\\1"),
         cond = stringr::str_c(
@@ -86,26 +87,29 @@ get_conditions_from_metadata <- function(data_frame,
 
   }
 
+  conditions_list
+
+  # Legado de odytools. Desactivado mientras se piensa sobre una gestíon más global.
   # We need to check if the conditions are actually filterable since variables
   # outside the data_frame can be used in the conditions. This is not suported
   # by the current implementation because ody_verify_completeness (the function
   # in charge of checking the conditions) only checks for the conditional presence
   # of variables in the current data_frame.
 
-  secure_filter_conditions <- purrr::possibly(filter_condition)
-
-  ok_index <- purrr::map_lgl(
-    conditions_list,
-    ~ secure_filter_conditions(data_frame, .) |>
-      is.data.frame()
-  )
-
-  if (sum(ok_index) < length(conditions_list)) {
-    warning(
-      "Some conditions are not filterable because they use variables not present in the data frame. These conditions will be ignored."
-    )
-  }
-
-  conditions_list[ok_index]
+  # secure_filter_conditions <- purrr::possibly(filter_condition)
+  #
+  # ok_index <- purrr::map_lgl(
+  #   conditions_list,
+  #   ~ secure_filter_conditions(data_frame, .) |>
+  #     is.data.frame()
+  # )
+  #
+  # if (sum(ok_index) < length(conditions_list)) {
+  #   warning(
+  #     "Some conditions are not filterable because they use variables not present in the data frame. These conditions will be ignored."
+  #   )
+  # }
+  #
+  # conditions_list[ok_index]
 
 }

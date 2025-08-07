110184877225ce0af05969995b66918f5bbd78d5
garcia-galea <eduardogarcia@vhio.net>
2025-08-07 16:04:40 +0200

    Refactor plausibility checks and update naming
    Renamed all instances of 'plausability' to 'plausibility' for consistency. Refactored the plausibility verification logic to improve candidate argument matching and metadata handling. Updated the documentation and function signatures accordingly. Modified the structure and creation of the plausibility_verifications_master object and its supporting functions. Removed obsolete files and updated the .rda data object.

--- R/plausibility_general.R
+++ R/plausibility_general.R
@@ -1,8 +1,7 @@
 # Plausability related general functions
 
 
-# Internal helper to filter issues from the verified data and glue the issue
-# text
+# Internal helper to filter issues from the verified data and glue the issue text
 filter_issues <- function(verified_data, issue_text) {
 
   verified_data |>
@@ -24,168 +23,154 @@ filter_issues <- function(verified_data, issue_text) {
 
 }
 
-
-
-
-# Helper functions to detect applicable verifications
-determine_verification_match <- function(verification, metadata) {
-
-  involved_variables <- verification$involved_variables[[1]]
+# Internal helper to find valid set of arguments for each verification.
+find_valid_candidates <- function(
+    arguments_metadata,
+    candidates_mapping,
+    complexity,
+    metadata) {
 
   candidates_list <-
-    purrr::map(
-      involved_variables$field_alias,
-      ~ metadata |>
-        dplyr::filter(field_name %in% .) |>
-        dplyr::select(
-          field_name,
-          form_name,
-          field_type,
-          select_choices_or_calculations,
-          text_validation_type_or_show_slider_number
-        )
+    purrr::map(1:nrow(candidates_mapping), ~ candidates_mapping[., ])
+
+  candidates_index <-
+    purrr::map_lgl(
+      candidates_list,
+      ~ all(. %in% metadata$field_name)
     )
 
-  # Se tiene que haber encontrado una y solo una candidato por variable involucrada
-  one_detection_per_field <- all(purrr::map_int(candidates_list, nrow) == 1)
+  present_candidates <- candidates_list[candidates_index]
 
-  if (!one_detection_per_field) return(NA)
+  if (length(present_candidates) == 0) return(NA)
 
-  # Todas las candidatas tienen que haber salido del mismo formulario si la
-  # complejidad es monoform
-  if (verification$complexity == "monoform") {
 
-    detected_forms <-
-      purrr::list_rbind(candidates_list)$form_name |>
-      unique() |>
-      length()
+  # If intraform complexity, we make sure al variables from the same set belong to
+  # the same form
+  if (complexity == "intraform") {
 
-    if (detected_forms != 1) return(NA)
+    # Number of diferent forms where the present candidates belong to
+    n_distinct_forms <-
+      purrr::map_int(
+        present_candidates,
+        ~ dplyr::filter(metadata, field_name %in% .) |>
+          dplyr::select(form_name) |>
+          unique() |>
+          nrow()
+      )
+
+    present_candidates <- present_candidates[n_distinct_forms == 1]
+
+    if (length(present_candidates) == 0) return(NA)
 
   }
 
-  involved_variables_list <-
+  present_candidates_match <-
     purrr::map(
-      1:nrow(involved_variables),
-      ~ involved_variables[.,]
-    )
-
-  verification_arg <-
-    purrr::map2(
-      candidates_list, involved_variables_list,
-      function(x, y) {
-
-        perfect_match <- c(
-          same_field_type = x$field_type == y$field_type,
-          same_choices =
-            x$select_choices_or_calculations == y$select_choices_or_calculations |
-            (is.na(x$select_choices_or_calculations) & is.na(y$select_choices_or_calculations)),
-          same_validation =
-            x$text_validation_type_or_show_slider_number == y$text_validation_type_or_show_slider_number |
-            (is.na(x$text_validation_type_or_show_slider_number) & is.na(y$text_validation_type_or_show_slider_number))
-        ) |> all()
-
-        tibble::tibble(
-          verification_argument = y$verification_argument,
-          detected_field = x$field_name,
-          perfect_match
-        )
-
+      present_candidates,
+      function(candidates_set) {
+        purrr::map2(
+          candidates_set, names(candidates_set),
+          ~ dplyr::filter(metadata, field_name == .x) |>
+            dplyr::mutate(
+              argument = .y
+            ) |>
+            dplyr::select(
+              "argument",
+              field_name = "field_name",
+              field_type_cand = "field_type",
+              field_choices_cand = "select_choices_or_calculations",
+              field_validation_cand = "text_validation_type_or_show_slider_number"
+            )
+        ) |>
+          purrr::list_rbind()
       }
     ) |>
-    purrr::list_rbind()
-
-
-  if (!all(verification_arg$perfect_match)) return(NA)
-
-  tibble::tibble(
-    verification,
-    verification_match = TRUE,
-    verification_arg = list(
-      verification_arg[, -3] |>
-        tidyr::pivot_wider(
-          names_from = verification_argument,
-          values_from = detected_field
-        )
+    purrr::map(
+      ~ dplyr::left_join(., arguments_metadata, by = "argument") |>
+        dplyr::mutate(
+          #All NA's are set to character so they return TRUE when comparing
+          # arguments with candidates
+          dplyr::across(tidyselect::everything(), ~ tidyr::replace_na(., "NA")),
+          type_ok = field_type_cand == field_type,
+          choices_ok = field_choices_cand == field_choices,
+          validation_ok = field_validation_cand == field_validation,
+          field_match = type_ok & choices_ok & validation_ok
+        )|>
+        dplyr::select("argument", "field_name", "field_match")
     )
-  )
 
+
+  valid_candidates_index <-
+    purrr::map_lgl(present_candidates_match, ~all(.$field_match))
+
+  valid_candidates <- present_candidates[valid_candidates_index]
+
+  if (length(valid_candidates) == 0) return(NA)
+
+  valid_candidates
 
 }
 
-
-#' Seek Issues in REDCap Data Based on Verifications
+#' Check Plausibility of REDCap Data
 #'
-#' This function processes REDCap data to detect applicable verification rules,
-#' optionally runs these verifications on the data, and returns the results.
+#' This function evaluates a REDCap dataset for plausibility issues by
+#' applying a series of predefined verifications. It identifies valid
+#' candidate fields based on metadata consistency, executes respective
+#' verification functions, and returns detected issues.
 #'
-#' @param rc_data A data frame containing REDCap data with associated metadata.
-#' @param run_verifications Logical, whether to execute the detected verifications on the data (default `TRUE`).
-#' @param return_no_issues_verif Logical, if `TRUE`, return all verification results including those with no issues; otherwise, only return those with detected issues (default `FALSE`).
+#' @param rc_data A data frame representing the REDCap export data. It must
+#'   be obtained with \code{odytools::ody_rc_import} and contain a "metadata"
+#'   attribute describing the data dictionary.
 #'
-#' @return A tibble of verifications applied to the data:
-#'   - If `run_verifications = TRUE`, a tibble with verification function names, arguments, descriptions, issues found, and count of issues.
-#'   - If `run_verifications = FALSE`, a tibble of detected verification rules that match the data.
+#' @return A tibble with columns:
+#' \describe{
+#'   \item{verif_fn}{The name of the verification function applied.}
+#'   \item{verif_arg}{The valid candidate arguments used for verification.}
+#'   \item{description}{Description of the verification.}
+#'   \item{n_issues}{Number of issues detected by the verification.}
+#'   \item{issues}{A list-column of data frames detailing detected issues.}
+#' }
 #'
 #' @details
-#' The function identifies applicable verifications based on the data's metadata,
-#' matches verification arguments, and can run the verifications to detect data problems.
-#' Verifications that do not apply or fail to match are excluded.
+#' The function uses global object `plausibility_verifications_master` to find
+#' applicable verifications. It filters candidates by metadata consistency and
+#' complexity ("intraform" constraints). Each verification function is called
+#' with its respective arguments on `rc_data`, and the resulting issues are
+#' collected.
 #'
 #' @export
-argos_check_plausability <- function(
-    rc_data,
-    run_verifications = TRUE,
-    return_no_issues_verif = FALSE
-
-) {
+argos_check_plausibility <- function(rc_data) {
 
   rc_data_expr <- rlang::enexpr(rc_data)
 
+  metadata <- attr(rc_data, "metadata")
+
   detected_verifications <-
-    purrr::map(
-      1:nrow(plausability_verifications_master),
-      ~ plausability_verifications_master[., ]
-    ) |>
-    purrr::map(determine_verification_match, attr(rc_data, "metadata")) |>
-    purrr::list_rbind() |>
-    dplyr::filter(verification_match) |>
+    plausibility_verifications_master |>
     dplyr::mutate(
-      verification_fn = stringr::str_c(verification, "_", version)
-    ) |>
-    dplyr::select(verification_fn, verification_arg, description)
-
-  if (run_verifications) {
-
-    runned_verifications <-
-      detected_verifications |>
-      dplyr::mutate(
-        issues = purrr::map2(
-          verification_fn, verification_arg,
-          ~ do.call(.x, c(.y, rc_data = rc_data_expr))
-        )
-      ) |>
-      dplyr::mutate(
-        n_issues = purrr::map_int(issues, nrow),
-        .before = issues
+      valid_candidates = purrr::pmap(
+        tibble::tibble(arguments_metadata, candidates_mapping, complexity),
+        find_valid_candidates,
+        metadata
       )
+    ) |>
+    dplyr::filter(!is.na(.data[["valid_candidates"]])) |>
+    dplyr::mutate(
+      verif_fn = stringr::str_c(.data[["id"]], "_", .data[["version"]])
+    ) |>
+    dplyr::select("verif_fn",  verif_arg = "valid_candidates", "description") |>
+    tidyr::unnest("verif_arg")
 
-    if (return_no_issues_verif) {
-
-      runned_verifications
-
-    } else {
-
-      runned_verifications |>
-        dplyr::filter(n_issues > 0)
-
-    }
-
-
-  } else {
-
-    detected_verifications
-
-  }
+  detected_verifications |>
+    dplyr::mutate(
+      issues = purrr::map2(
+        verif_fn, verif_arg,
+        ~ do.call(.x, c(.y, rc_data = rc_data_expr))
+      )
+    ) |>
+    dplyr::mutate(
+      n_issues = purrr::map_int(issues, nrow),
+      .before = "issues"
+    )
 
 }
--- data-raw/dataset_plausibility_verifications_master.R
+++ data-raw/dataset_plausibility_verifications_master.R
@@ -1,56 +1,63 @@
 
 # This functions is completely specific to the creation of the verifications
 # master. That is why it is here.
-format_verification_info <- function(arguments, candidates) {
+create_arguments_metadata <- function(arguments) {
 
   splited_arguments <-
     arguments |>
     stringr::str_split("\n") |>
     unlist()
 
-  arguments_tbl <-
-    purrr::map(
-      splited_arguments,
-      function(x) {
+  purrr::map(
+    splited_arguments,
+    function(x) {
 
-        argument <- stringr::str_split(x, "::") |> unlist()
+      argument <- stringr::str_split(x, "::") |> unlist()
 
-        metadata <-
-          odytools::ody_rc_get_metadata(
-            Sys.getenv(
-              stringr::str_c(argument[3], "_api_key") |>
-                stringr::str_to_upper()
-            )
-          ) |>
-          dplyr::filter(field_name == argument[2])
+      metadata <-
+        odytools::ody_rc_get_metadata(
+          Sys.getenv(
+            stringr::str_c(argument[3], "_api_key") |>
+              stringr::str_to_upper()
+          )
+        ) |>
+        dplyr::filter(field_name == argument[2])
 
-        tibble::tibble(
-          argument = argument[1],
-          field_type = metadata$field_type,
-          field_choices = metadata$select_choices_or_calculations,
-          field_validation = metadata$text_validation_type_or_show_slider_number,
-        )
+      tibble::tibble(
+        argument = argument[1],
+        field_type = metadata$field_type,
+        field_choices = metadata$select_choices_or_calculations,
+        field_validation = metadata$text_validation_type_or_show_slider_number,
+      )
 
-      }
-    ) |>
+    }
+  ) |>
     purrr::list_rbind()
 
-  candidates_groups <-
+}
+
+
+
+# This functions is completely specific to the creation of the verifications
+# master. That is why it is here.
+create_candidates_mapping <- function(candidates) {
+
     candidates |>
     stringr::str_split("\n") |>
     unlist() |>
-    purrr::map(~stringr::str_c("c(", ., ")") |> str2lang() |> eval())
-
-  candidate_fields <-
     purrr::map(
-      arguments_tbl$argument,
-      function(arg) {
-        purrr::map_chr(candidates_groups, ~ .[arg])
+      function(x) {
+        arg_vector <- stringr::str_c("c(", x, ")") |> str2lang() |> eval()
+        arg_names <- names(arg_vector)
+        names(arg_vector) <- NULL
+        purrr::map2(
+          arg_vector, arg_names,
+          ~ tibble::tibble("{.y}" := .x)
+        ) |>
+          purrr::list_cbind()
       }
-    )
-
-  arguments_tbl |>
-    dplyr::mutate(candidate_fields, .after = "argument")
+    ) |>
+    purrr::list_rbind()
 
 }
 
@@ -59,10 +66,11 @@ format_verification_info <- function(arguments, candidates) {
 plausibility_verifications_master <-
   readr::read_csv2(here::here("data-raw", "plausibility_verifications_master.csv")) |>
   dplyr::mutate(
-    arguments_info = purrr::map2(arguments, candidate_fields, format_verification_info)
+    arguments_metadata = purrr::map(arguments, create_arguments_metadata),
+    candidates_mapping = purrr::map(candidate_fields, create_candidates_mapping)
   ) |>
   dplyr::select(
-    id, version, description, complexity, arguments_info
+    id, version, description, complexity, arguments_metadata, candidates_mapping
   )
 
 
